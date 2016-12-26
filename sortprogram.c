e8f1f6831f444b86f3ea95255a3e14e2879b6ea5
chaosbringer <hoangtrung225@gmail.com>
2016-12-26 17:14:18 +0700

    modify output

--- sortprogram.c
+++ sortprogram.c
@@ -9,14 +9,14 @@ int main(int argc,char** argv){
     if(scanf("%d\n",&digitArray[lenArray]) == EOF) break;
     lenArray++;
     digitArray = realloc(digitArray, (lenArray+1)*sizeof(int));
-    printf("after realloc get %dth element value: %d\n",lenArray -1, digitArray[lenArray-1]);
     if(digitArray == NULL){
       printf("not enought memory function abort\n");
       return 0;
     }
   } while(1);
-
+  printf("before sorting\n");
   for(int i = 0; i < lenArray; i++) printf("%d\n", digitArray[i]);
   digitArray = insertionSort(digitArray, lenArray);
+  printf("after sorting\n");
   for(int i = 0; i < lenArray; i++) printf("%d\n", digitArray[i]);
 }
